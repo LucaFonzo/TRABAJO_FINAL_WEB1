7e2abd1ac921e420227d6636974b05584e518c94
Luca Fonzo <lucafonzo3110@gmail.com>
2022-06-27 11:00:36 -0300

    Tabla API

--- js/app.js
+++ js/app.js
@@ -13,11 +13,10 @@ const procesadores = document.querySelector("#procesadores");
 const tablaPc = document.querySelector("#tabla-pc");
 const btnx3 = document.querySelector('#btnx3');
 const btnBorrarTodo = document.querySelector("#btnBorrarTodo");
-
+const URL_API ="https://62b5b66042c6473c4b38869b.mockapi.io/tablaWeb1/datosTabla";
+let  id = 0;
 let valoresAlmacenados = [];
-const caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'l', 'o', 'p', 'q', 'r', 'v', 'w', 'y', 'x', 'z',0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//Eventos
-//Refresh captcha
+const caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'l', 'o', 'p', 'q', 'r', 'v', 'w', 'y', 'x', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 document.addEventListener('DOMContentLoaded', iniciarApp);
 function iniciarApp() {
   btnRefresh.addEventListener("click", generateCaptcha);
@@ -27,7 +26,6 @@ function iniciarApp() {
   generateCaptcha();
 }
 //Funciones
-//Captcha
 function generateCaptcha() {
   captchaValue.innerHTML = ""
   for (let i = 0; i < 6; i++) {
@@ -80,54 +78,122 @@ let pc = {
 btnFormPc.addEventListener('click', e => {
   e.preventDefault()
   let formDataPc = new FormData(formPc);
-  pc.procesador = formDataPc.get("procesador");
-  pc.motherboard = formDataPc.get("motherboard");
-  pc.gpu = formDataPc.get("gpu");
+  let procesador = formDataPc.get("procesador");
+  let motherboard = formDataPc.get("motherboard");
+  let gpu = formDataPc.get("gpu");
   let codigo = formDataPc.get("codigo");
   valoresAlmacenados.push(pc);
-  agregarElementos(pc, codigo);
+  enviarDatos(procesador,motherboard,gpu,codigo)
 })
 btnx3.addEventListener('click', e => {
   e.preventDefault()
   let formDataPc = new FormData(formPc);
-  pc.procesador = formDataPc.get("procesador");
-  pc.motherboard = formDataPc.get("motherboard");
-  pc.gpu = formDataPc.get("gpu");
+  let procesador = formDataPc.get("procesador");
+  let motherboard = formDataPc.get("motherboard");
+  let gpu = formDataPc.get("gpu");
   let codigo = formDataPc.get("codigo");
   for (let i = 0; i < 3; i++) {
     valoresAlmacenados.push(pc);
-    agregarElementos(pc,codigo);
+    enviarDatos(procesador, motherboard, gpu, codigo);
   }
 })
-function agregarElementos(pc,codigo) {
+function agregarElementos(datos) {
   const tr = document.createElement('tr');
-  if (codigo != '') {
+  if (datos.descuento != '') {
     tr.innerHTML = `
-  <td>${pc.procesador}</td>
-  <td>${pc.motherboard}</td>
-  <td>${pc.gpu}</td>
-  <td>${pc.precio}</td>
+  <td>${datos.procesador}</td>
+  <td>${datos.motherboard}</td>
+  <td>${datos.gpu}</td>
+  <td>${datos.precioTotal}</td>
   <td>15%</td>
+  <td><button class="btn-borrar"><i class="fa-solid fa-trash-can"></i></button><button class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></button></td>
   `;
     tr.classList.add('descuento');
   } else {
     tr.innerHTML = `
-  <td>${pc.procesador}</td>
-  <td>${pc.motherboard}</td>
-  <td>${pc.gpu}</td>
-  <td>${pc.precio}</td>
-  <td>0%</td>
+  <td>${datos.procesador}</td>
+  <td>${datos.motherboard}</td>
+  <td>${datos.gpu}</td>
+  <td>${datos.precioTotal}</td>
+  <td>0% </td>
+  <td><button class="btn-borrar"><i class="fa-solid fa-trash-can"></i></button><button class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></button></td>
   `;
   }
   tablaPc.appendChild(tr)
 }
 btnBorrarTodo.addEventListener('click', e => {
   e.preventDefault();
-  valoresAlmacenados = [];
-  borrarHTML()
+  borrarDatos()
 })
 function borrarHTML() {
   while (tablaPc.firstChild) {
     tablaPc.removeChild(tablaPc.firstChild);
 }
+}
+async function obtenerDatos() {
+  try {
+    let response = await fetch(URL_API); //GET URL_API
+    let json = await response.json(); // texto json a objeto
+    for (const datos of json) {
+      agregarElementos(datos)
+      id = datos.id
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+obtenerDatos()
+async function enviarDatos(procesador, mother, gpu, descuento) {
+  let pc = {
+    procesador: procesador,
+    motherboard: mother,
+    gpu: gpu,
+    precioTotal: 500,
+    descuento: descuento
+  }
+  try {
+    let response = await fetch(URL_API, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(pc)
+    });
+    if (response.status === 201) {
+      agregarElementos(pc)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+async function modificarDatos(procesador, mother, gpu, descuento) {
+  let pc = {
+    procesador: procesador,
+    motherboard: mother,
+    gpu: gpu,
+    precioTotal: 500,
+    descuento: descuento,
+  };
+  try {
+    let response = await fetch(URL_API, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(pc),
+    });
+    if (response.status === 200) {
+      agregarElementos(pc);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function borrarDatos() {
+    try {
+      let response = await fetch(`${URL_API} + ${pc.id}`, {
+        method: "DELETE",
+      });
+      if (response.status === 200) {
+        
+      }
+    } catch (error) {
+      console.log(error);
+    }
 }
